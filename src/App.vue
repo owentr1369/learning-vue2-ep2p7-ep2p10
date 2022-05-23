4c2327037e472a190a88331973694b389cd0149b
owentr1369 <tamvaa1306@gmail.com>
2022-05-23 22:13:08 +0700

    ep2 part 7

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1>HelloWorld</h1>
+    <div id="obj">
+      <h1>Bam nut de tang gia tri</h1>
+      <h2>{{ dem }}</h2>
+      <button v-on:click="handleIncrease">Increase</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      dem: 0,
+    };
+  },
+  methods: {
+    handleIncrease: function () {
+      this.dem = this.dem + 1;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
